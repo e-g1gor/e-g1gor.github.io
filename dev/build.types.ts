bb4d6a59ec3d3eabadc258120b893f5b54824763
user <user@example.com>
2022-02-02 22:00:31 +0300

    refactor: improve build script

--- dev/build.types.ts
+++ dev/build.types.ts
@@ -1,8 +1,30 @@
 export interface IArgs {
+  /**
+   * Enable HTML compiling from PugJS templates and Markdown
+   */
   html: boolean;
+  /**
+   * TODO: implement pdf export (puppeteer)
+   */
   pdf: boolean;
+  /**
+   * Enable CSS compiling from SASS sources
+   */
   css: boolean;
+  /**
+   * TODO: implement livereload for development
+   */
   watch: boolean;
+  /**
+   * CV template name (stored in `./src/cv-data/cvs`)
+   */
   cv: string;
+  /**
+   * Output directory path
+   */
   o: string;
+  /**
+   * Log level: 1 - info, 2 - debug, 3 - verbose
+   */
+  l: 0 | 1 | 2;
 }
